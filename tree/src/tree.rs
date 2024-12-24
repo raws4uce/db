7739ab430661fa2058afb8bbef6b240a7b3894ec
raws4uce <sauravmahay10@gmail.com>
2024-12-24 20:05:27 +0000

    todo Deletion

--- tree/src/tree.rs
+++ tree/src/tree.rs
@@ -2,43 +2,44 @@ use anyhow::{anyhow, Result};
 use dump::util;
 pub struct TreeNode {
     id: usize,
-    data: Vec<Option<usize>>,             // Data stored in this node
-    children: Option<Vec<Box<TreeNode>>>, // Children nodes
-    parent: Option<*mut TreeNode>,        // Raw pointer to avoid lifetime conflicts
+    pub data: Vec<usize>,                     // Data stored in this node
+    pub children: Option<Vec<Box<TreeNode>>>, // Children nodes
+    pub parent: Option<*mut TreeNode>,        // Raw pointer to avoid lifetime conflicts
 }
 
 impl TreeNode {
     pub fn new_root(data: usize) -> TreeNode {
         TreeNode {
             id: util::id_init(),
-            data: vec![Some(data)],
+            data: vec![data],
             children: None,
             parent: None,
         }
     }
-    pub fn node_innit(&mut self, data: usize) -> TreeNode {
+    pub fn node_innit(&mut self, data: Vec<usize>) -> TreeNode {
         TreeNode {
             id: util::id_init(),
-            data: vec![Some(data)],       // Directly initialise with the data
+            data,                         // Directly initialise with the data
             children: None,               // No children for a new node
             parent: Some(self as *mut _), // Use raw pointer to self for parent
         }
     }
 
-    pub fn look_for(&self, data: usize) -> Option<Vec<Option<usize>>> {
-        if let Ok(_) = self.data.binary_search(&Some(data)) {
+    pub fn look_for(&self, data: usize) -> Option<Vec<usize>> {
+        if let Ok(_) = self.data.binary_search(&data) {
             return Some(self.data.clone());
         }
+
         let index = self
             .data
             .iter()
             .enumerate()
-            .find(|(_, dat)| dat.as_ref().map_or(false, |d| &data < d))
+            .find(|(_, dat)| Some(dat).map_or(false, |d| &data < d))
             .map(|(i, _)| i)
             .unwrap_or_else(|| self.data.len());
         if let Some(children) = self.children.as_ref() {
             if let Some(child) = children.get(index) {
-                child.look_for(data);
+                return child.look_for(data);
             }
         }
         None
@@ -49,11 +50,14 @@ impl TreeNode {
             .data
             .iter()
             .enumerate()
-            .find(|(_, dat)| dat.as_ref().map_or(false, |d| &data < d))
+            .find(|(_, dat)| Some(dat).map_or(false, |d| &data < d))
             .map(|(i, _)| i)
             .unwrap_or_else(|| self.data.len());
 
-        let new_child = self.node_innit(data); //satisfies compiler...
+        //a little inneficient but its like 4 u64||u32s, no biggie
+        let mut v = self.data.clone();
+        v.push(data);
+        let new_child = self.node_innit(v); //satisfies compiler...
 
         if let Some(children) = self.children.as_mut() {
             if let Some(child) = children.get_mut(index) {
@@ -63,42 +67,58 @@ impl TreeNode {
             }
         } else {
             if self.data.len() == 4 {
-                self.data.push(Some(data));
+                self.data.push(data);
                 self.split()?;
             } else {
-                self.data.push(Some(data));
+                self.data.push(data);
                 self.data.sort_by(|a, b| a.cmp(b));
             }
         }
         Ok(())
     }
+
     pub fn delete(&mut self, data: usize) -> Result<()> {
-        if let Ok(i) = self.data.binary_search(&Some(data)) {
-            self.data.remove(i);
+        if let Ok(i) = self.data.binary_search(&data) {
+            if self.parent.is_none() {
+                //leaf me
+                self.data.remove(i);
+            } else {
+                //replace entry with predecessor
+                let pre = self.data[i - 1];
+                self.data[i] = pre;
+                self.children.as_mut().unwrap()[i].delete(pre)?;
+            }
         } else {
             let index = self
                 .data
                 .iter()
                 .enumerate()
-                .find(|(_, dat)| dat.as_ref().map_or(false, |d| &data < d))
+                .find(|(_, dat)| Some(dat).map_or(false, |d| &data < d))
                 .map(|(i, _)| i)
                 .unwrap_or_else(|| self.data.len());
+
             if let Some(children) = self.children.as_mut() {
                 if let Some(child) = children.get_mut(index) {
                     child.delete(data)?;
+                    if child.data.len() < 2 {
+                        self.underflow(index)?;
+                    }
                 }
             }
         }
         Err(anyhow!("THIS IS AN ERROR"))
     }
+    fn underflow(&mut self, index: usize) -> Result<()> {
+        Ok(())
+    }
     pub fn update() {}
     pub fn traverse() {}
     pub fn split(&mut self) -> Result<()> {
         let mid_index = self.data.len() / 2;
-        let middle = self.data[mid_index].take().unwrap();
+        let middle = self.data[mid_index];
 
-        let left_data: Vec<Option<usize>> = self.data.drain(..mid_index).collect();
-        let right_data: Vec<Option<usize>> = self.data.drain(mid_index..).collect();
+        let left_data: Vec<usize> = self.data.drain(..mid_index).collect();
+        let right_data: Vec<usize> = self.data.drain(mid_index..).collect();
 
         let left_child: Option<Vec<Box<TreeNode>>> = self
             .children
@@ -124,7 +144,7 @@ impl TreeNode {
             });
             let new_root = TreeNode {
                 id: util::id_init(),
-                data: vec![Some(middle)],
+                data: vec![middle],
                 children: Some(vec![left_node, right_node]),
                 parent: None,
             };
@@ -150,7 +170,7 @@ impl TreeNode {
             });
 
             unsafe {
-                (*parent).data.push(Some(middle));
+                (*parent).data.push(middle);
                 (*parent).data.sort();
                 if let Some(children) = (*parent).children.as_mut() {
                     let index = children.iter().position(|c| c.id == self.id).unwrap_or(1);
@@ -159,6 +179,12 @@ impl TreeNode {
                     children.insert(index + 1, right_node);
                 } else {
                     return Err(anyhow!("parent has NO child Vec"));
+                }
+            }
+
+            unsafe {
+                if (*parent).data.len() > 4 {
+                    (*parent).split()?;
                 }
             }
 
--- tree/tests/tree.rs
+++ tree/tests/tree.rs
@@ -1,5 +1,8 @@
 #[cfg(test)]
 mod tests {
+    use std::vec;
+
+    use anyhow::{Ok, Result};
     use tree::tree::TreeNode;
 
     #[test]
@@ -7,12 +10,74 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
     #[test]
-    fn std_insertion() {
+    fn root_init() -> Result<()> {
         let root = TreeNode::new_root(5);
-        println!("{:?}", root.look_for(5));
-        assert!(
-            format!("{:?}", Some([Some(5)])),
-            format!("{:?}", root.look_for(5))
-        );
+        assert_eq!(root.data[0], 5);
+        Ok(())
+    }
+    #[test]
+    fn node_init() -> Result<()> {
+        let mut root = TreeNode::new_root(2);
+        let n0 = root.node_innit(vec![0, 1, 2, 3]);
+        let n1 = root.node_innit(vec![5, 6, 7, 8]);
+        let n2 = root.node_innit(vec![10, 11, 12, 13]);
+        let n3 = root.node_innit(vec![15, 16, 17, 18]);
+        let n4 = root.node_innit(vec![20, 21, 22, 23]);
+        //root characteristics
+        let d = Some(vec![4, 9, 14, 19]);
+        root.data = d.expect("root node cant hold Option<Vec<4* Option<usize>>>");
+        root.children = Some(vec![
+            Box::new(n0),
+            Box::new(n1),
+            Box::new(n2),
+            Box::new(n3),
+            Box::new(n4),
+        ]);
+        Ok(())
+    }
+    #[test]
+    fn searchin() -> Result<()> {
+        let mut root = TreeNode::new_root(2);
+        let n0 = root.node_innit(vec![0, 1, 2, 3]);
+        let n1 = root.node_innit(vec![5, 6, 7, 8]);
+        let n2 = root.node_innit(vec![10, 11, 12, 13]);
+        let n3 = root.node_innit(vec![15, 16, 17, 18]);
+        let n4 = root.node_innit(vec![20, 21, 22, 23]);
+        //root characteristics
+        let d = Some(vec![4, 9, 14, 19]);
+        root.data = d.expect("root node cant hold Option<Vec<4* Option<usize>>>");
+        root.children = Some(vec![
+            Box::new(n0),
+            Box::new(n1),
+            Box::new(n2),
+            Box::new(n3),
+            Box::new(n4),
+        ]);
+        assert_eq!(root.look_for(19), Some(vec![4, 9, 14, 19]));
+        assert_eq!(root.look_for(17), Some(vec![15, 16, 17, 18]));
+        assert_eq!(root.look_for(3434), None);
+        Ok(())
+    }
+    #[test]
+    fn insert_overflow_root() -> Result<()> {
+        let mut root = TreeNode::new_root(2);
+        let n0 = root.node_innit(vec![0, 1, 2, 3]);
+        let n1 = root.node_innit(vec![5, 6, 7, 8]);
+        let n2 = root.node_innit(vec![10, 11, 12, 13]);
+        let n3 = root.node_innit(vec![15, 16, 17, 18]);
+        let n4 = root.node_innit(vec![20, 21, 22, 23]);
+        //root characteristics
+        let d = Some(vec![4, 9, 14, 19]);
+        root.data = d.expect("root node cant hold Option<Vec<4* Option<usize>>>");
+        root.children = Some(vec![
+            Box::new(n0),
+            Box::new(n1),
+            Box::new(n2),
+            Box::new(n3),
+            Box::new(n4),
+        ]);
+        root.stick_in(24)?;
+        assert_eq!(vec![14], root.data);
+        Ok(())
     }
 }
